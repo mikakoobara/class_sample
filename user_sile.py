7241c6b79fc052e251e53028005ebb90dd6debf3
obaramikako <quartersilver@gmail.com>
2018-11-18 14:19:18 +0900

    属性、リファクタリング

--- user_sile.py
+++ user_sile.py
@@ -1,18 +1,25 @@
 class User:
-    def __init__(self,name):
+    def __init__(self,name, age, country):
         self.name = name
+        self.age = age
+        self.country = country
 
 
 if __name__ == "__main__":
-    user1 = User("Bob")   #Userクラスのインスタンス化
+    Bob = User("Bob", 15, "Unitedstats")   #Userクラスのインスタンス化
 
-    print(user1)
-    print(user1.name)
+    print(Bob)
+    print(Bob.name)
+    print(Bob.age)
+    print(Bob.country)
 
-    user2 = User("Tom")
-    print(user2)
-    print(user2.name)
+    Tom = User("Tom", 57, "Unitedstats")
+    print(Tom)
+    print(Tom.name)
+    print(Tom.country)
 
-    user3 = User("Ken")
-    print(user3)
-    print(user3.name)
+    Ken = User("Ken", 49, "JApan")
+    print(Ken)
+    print(Ken.name)
+    print(Ken.country)
+
