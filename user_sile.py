6864db838ecb890be3d7a23973d41f830c1b9609
obaramikako <quartersilver@gmail.com>
2018-11-18 14:54:32 +0900

    クラスのインスタンスメソッド

--- user_sile.py
+++ user_sile.py
@@ -1,25 +1,36 @@
 class User:
     def __init__(self,name, age, country):
+        # インスタンス変数
         self.name = name
         self.age = age
         self.country = country
 
 
+    def display_profile(self):
+        #diplay_profile()はUserくらすのインスタンスメソッド
+        print(f"{self.name} 国籍: {self.country} 年齢:{self.age}歳")
+
+    def change_nathionality(self,coutry_name):
+         self.country = coutry_name
+
+
 if __name__ == "__main__":
-    Bob = User("Bob", 15, "Unitedstats")   #Userクラスのインスタンス化
+    bob = User("Bob", 15, "USA")   #Userクラスのインスタンス化
+    bob.display_profile()
+    bob.change_nathionality("China")
+    bob.display_profile()
 
-    print(Bob)
-    print(Bob.name)
-    print(Bob.age)
-    print(Bob.country)
 
-    Tom = User("Tom", 57, "Unitedstats")
-    print(Tom)
-    print(Tom.name)
-    print(Tom.country)
+    #print(bob.name)
 
-    Ken = User("Ken", 49, "JApan")
-    print(Ken)
-    print(Ken.name)
-    print(Ken.country)
+    tom = User("Tom", 57, "USA")
+    tom.display_profile()
+    tom.change_nathionality("China")
+    tom.display_profile()
 
+
+
+    ken = User("Ken", 49, "JP")
+    ken.display_profile()
+    ken.change_nathionality("UK")
+    ken.display_profile()
